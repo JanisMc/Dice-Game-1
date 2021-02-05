522e45b1cabb58f53f64901ebb7b41fa403596ce
Janis <janismcluckie@googlemail.com>
2021-02-05 15:14:00 +0000

    added if statement to display dice image dependent on roll result

--- script.js
+++ script.js
@@ -6,5 +6,20 @@ image.style.display = "none";
 button.addEventListener("click", () => {
     let press = Math.ceil(Math.random() * 6);
     console.log(press);
+    
+    image.style.display = "flex";
 
+    if (press === 1) {
+        image.src = "./img/dice1.png"; 
+    } else if (press === 2) {
+        image.src =  "./img/dice2.png";
+    } else if (press === 3) {
+        image.src =  "./img/dice3.png";
+    } else if (press === 4) {
+        image.src =  "./img/dice4.png";
+    } else if (press === 5) {
+        image.src =  "./img/dice5.png";
+    } else if (press === 6) {
+        image.src =  "./img/dice6.png";
+    }
 })
