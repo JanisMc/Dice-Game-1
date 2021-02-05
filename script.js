f6de5cb3869c4e05d4eee526568ff02fc2829021
Janis <janismcluckie@googlemail.com>
2021-02-05 16:28:41 +0000

    added variables to html/js for score/win/lose

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 const button = document.getElementById("button");
 const image = document.getElementById("dice");
+const score = document.getElementById("score")
+const win = document.getElementById("win");
+const lose = document.getElementById("lose");
+
+let runningScore = 0;
 
 image.style.display = "none";
+win.style.display = "none";
+lose.style.display = "none";
 
 button.addEventListener("click", () => {
     let press = Math.ceil(Math.random() * 6);
     console.log(press);
-    
+
     image.style.display = "flex";
 
     if (press === 1) {
-        image.src = "./img/dice1.png"; 
+        image.src = "./img/dice1.png";
+        lose.style.display = "block"; 
     } else if (press === 2) {
         image.src =  "./img/dice2.png";
     } else if (press === 3) {
@@ -22,4 +30,14 @@ button.addEventListener("click", () => {
     } else if (press === 6) {
         image.src =  "./img/dice6.png";
     }
+
+    runningScore += press;
+    
+    score.textContent = runningScore;
+
+    if (runningScore > 20) {
+        win.style.display = "block";
+    } 
+
 })
+
