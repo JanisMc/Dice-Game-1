c94ff635818e28896ba43a8e9e4459557014b08f
Janis <janismcluckie@googlemail.com>
2021-02-05 14:45:24 +0000

    added html to src img & js to hide image at start

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const button = document.getElementById("button");
+const image = document.getElementById("die1");
+
+image.style.display = "none";
 
 button.addEventListener("click", () => {
     let press = Math.ceil(Math.random() * 6);
